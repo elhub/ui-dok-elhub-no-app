905081047f482293070d9a958a89fbdc1a371281
Pål Oskar Engen <pal.engen@statnett.no>
2023-08-31 13:48:56 +0200

    Initialize repo with sanity studio

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
+import { createClient } from "next-sanity";
+
+const client = createClient({
+  projectId: "ovv3c751",
+  dataset: "production",
+  apiVersion: "2023-08-31",
+  useCdn: false,
+});
+
 export default function IndexPage({ pets }) {
   return (
     <>
@@ -5,7 +14,7 @@ export default function IndexPage({ pets }) {
         <h1>Sanity + Next.js</h1>
       </header>
       <main>
-        <h2>Pets</h2>
+        <h2>pets</h2>
         {pets.length > 0 && (
           <ul>
             {pets.map((pet) => (
@@ -22,10 +31,7 @@ export default function IndexPage({ pets }) {
         {!pets.length > 0 && (
           <div>
             <div>¯\_(ツ)_/¯</div>
-            <p>
-              Your data will show up here when you've configured everything
-              correctly
-            </p>
+            <p>Your data will show up here when you've configured everything correctly</p>
           </div>
         )}
       </main>
@@ -34,20 +40,11 @@ export default function IndexPage({ pets }) {
 }
 
 export async function getStaticProps() {
-  const pets = [
-    /* {
-      _createdAt: "2022-03-08T09:28:00Z",
-      _id: "1f69c53d-418a-452f-849a-e92466bb9c75",
-      _rev: "xnBg0xhUDzo561jnWODd5e",
-      _type: "pet",
-      _updatedAt: "2022-03-08T09:28:00Z",
-      name: "Bamse"
-    } */
-  ];
+  const pets = await client.fetch(`*[_type == "pet"]`);
 
   return {
     props: {
-      pets
-    }
+      pets,
+    },
   };
 }
